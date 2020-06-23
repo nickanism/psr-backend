8b6511fda7543be83ddb806e144376680419ac9c
Woo Hyun An <nickanism@gmail.com>
2020-06-23 15:13:10 +0900

    WIP

--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from rest_framework     import generics
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
-from user.serializers   import UserSerializer
+from user.serializers   import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
