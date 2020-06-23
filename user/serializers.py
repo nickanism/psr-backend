afb6ea6c12aa92a53f63fa0356b8ed91d6731416
Woo Hyun An <nickanism@gmail.com>
2020-06-23 15:11:39 +0900

    WIP

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import get_user_model
-from django.util.translation import ugettext_lazy as _
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import ugettext_lazy as _
 
 from rest_framework      import serializers
 
